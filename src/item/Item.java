d55c52026a8ab24821817a7441ca60ee6354f62a
nubo92 <kks576@naver.com>
2025-11-05 20:05:40 +0900

    shop 수정

--- src/item/Item.java
+++ src/item/Item.java
@@ -9,4 +9,6 @@ public abstract class Item {
     public String getName() {
         return name;
     }
+
+
 }
--- src/region/Dungeon.java
+++ src/region/Dungeon.java
@@ -1,10 +1,14 @@
 package region;
 
+import unit.Enemy;
 import unit.Player;
+
+import java.util.Random;
 
 public class Dungeon extends Region {
     @Override
     public void enter(Player player) {
+
 
     }
 }
